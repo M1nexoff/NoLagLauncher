f42d83aeb01bea61265562513fe4183c2adc304e
M1nexoff <aiskandarxojayev1@gmail.com>
2026-06-28 13:31:53 +0500

    Use aiskandarxojayev@gmail.com in source headers and README
    Match the contact email already updated in the legal documents.

--- app/src/main/java/uz/m1nex/nolaglauncher/data/local/dao/AppDao.kt
+++ app/src/main/java/uz/m1nex/nolaglauncher/data/local/dao/AppDao.kt
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2026 A'zamxo'ja Iskandarxo'jayev <devasgardia@gmail.com>
+// SPDX-FileCopyrightText: 2026 A'zamxo'ja Iskandarxo'jayev <aiskandarxojayev@gmail.com>
 // SPDX-License-Identifier: AGPL-3.0-only
 
 package uz.m1nex.nolaglauncher.data.local.dao
